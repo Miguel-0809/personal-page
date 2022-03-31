f36da116fe89d016391b38ffef4a06d82ff0fe86
Miguel Angel Paucar Navarro <nexl1020@gmail.com>
2022-03-30 23:47:50 -0500

    Scroll reveal added

--- src/index.js
+++ src/index.js
@@ -10,3 +10,47 @@ const responsiveToggle=()=>{
 
     console.log('aea')
 }
+
+//SCROLL REVEAL
+window.sr = ScrollReveal();
+
+
+
+if(screen.availWidth>=780){
+    sr.reveal('.home-intro', {
+        duration:3000,
+        origin: 'bottom',
+        distance: '100px',
+        reset: true
+    });
+
+    sr.reveal('.top-bar', {
+        duration:3000,
+        origin: 'bottom',
+        distance: '-100px',
+        reset: true
+    });
+}
+
+
+sr.reveal('.inicio-content', {
+    duration: 4000,
+    origin: 'left',
+    distance: '400px',
+    reset: true
+});
+
+sr.reveal('.project-card', {
+    duration: 4000,
+    origin: 'right',
+    distance: '400px',
+    reset: true
+});
+
+sr.reveal('.form-contact', {
+    duration: 3000,
+    origin: 'left',
+    distance: '400px',
+    reset: true
+});
+
